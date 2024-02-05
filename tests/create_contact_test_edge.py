59918c94b30f007fae0c386678a8042b3fe5f277
Rudolf <Rudolf@01-19-RUDOLFPAR>
2024-02-05 22:47:30 +0700

    Functional test

--- tests/create_contact_test_edge.py
+++ tests/create_contact_test_edge.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-class CreateContactTestCase(unittest.TestCase):
+class CreateContactEdgeTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
