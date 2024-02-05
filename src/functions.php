5370b5419983f35d6043aba61ff676db66ba4f68
Rudolf <Rudolf@01-19-RUDOLFPAR>
2024-02-05 22:08:03 +0700

    coba

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 function pdo_connect(){
     $DATABASE_HOST = 'mysql';
     $DATABASE_USER = 'root';
-    $DATABASE_PASS = 'mysqlpassword';
+    $DATABASE_PASS = 'aman123';
     $DATABASE_NAME = 'badcrud';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
