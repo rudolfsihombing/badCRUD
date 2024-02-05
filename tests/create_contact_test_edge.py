3c0b1776920d79af2e9d4fb3cffd500de356da2e
Rudolf <Rudolf@01-19-RUDOLFPAR>
2024-02-05 22:44:59 +0700

    Edge Test

---
+++ tests/create_contact_test_edge.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class CreateContactTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        option = webdriver.EdgeOptions()
+        option.add_argument('--headless')
+        cls.browser = webdriver.Edge(options=option)
+        try:
+            cls.url = os.environ['URL']
+        except:
+            cls.url = "http://localhost"
+            
+    def test_create_contact(self):
+        login_url = self.url + '/login.php'
+        self.browser.get(login_url)
+        self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
+        self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
+        self.browser.find_element(By.TAG_NAME, 'button').click()
+        
+        btn_create = self.browser.find_element(By.XPATH, "//a[contains(@class, 'create-contact')]")
+        btn_create.click()
+
+        self.browser.find_element(By.ID, 'name').send_keys('Livia')
+        self.browser.find_element(By.ID, 'email').send_keys('livia@example.com')
+        self.browser.find_element(By.ID, 'phone').send_keys('+6281234321127')
+        self.browser.find_element(By.ID, 'title').send_keys('General')
+
+        self.browser.find_element(By.CSS_SELECTOR, 'input[type="submit"]').click()
+
+        index_page_title = "Dashboard"
+        actual_title = self.browser.title
+        self.assertEqual(index_page_title, actual_title)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2, warnings='ignore')
